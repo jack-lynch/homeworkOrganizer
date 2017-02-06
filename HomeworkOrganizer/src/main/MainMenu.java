204b35d24f915fa7831113e3dffe8ab3cda27529
100735 <100735@10.113.57.63>
2017-02-06 12:32:12 -0500

    size change of the main window

--- HomeworkOrganizer/src/main/MainMenu.java
+++ HomeworkOrganizer/src/main/MainMenu.java
@@ -37,7 +37,7 @@ public class MainMenu extends JFrame {
 	 */
 	public MainMenu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
