5ef17fcf4489c7226a82a441d68cda527d1bf8b2
100735 <100735@10.113.57.63>
2017-02-07 12:16:36 -0500

    created hashmap

--- HomeworkOrganizer/src/main/Schedule.java
+++ HomeworkOrganizer/src/main/Schedule.java
@@ -1,16 +1,11 @@
 package main;
 
+import java.util.HashMap;
+
 public class Schedule {
 	
-	private int period1;
-	private int period2;
-	private int period3;
-	private int period4;
-	private int period5;
-	private int period6;
-	private int period7;
-	private int period8;
 	
 	
+	HashMap<Integer, String> courseList = new HashMap<Integer, String>();
 
 }
---
+++ HomeworkOrganizer/src/main/SchedulePanel.java
@@ -0,0 +1,5 @@
+package main;
+
+public class SchedulePanel extends MainPanel {
+
+}
