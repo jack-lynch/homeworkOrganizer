a18ac19f32d359e9c242a1da594dd59fdfe7cd5e
100735 <100735@10.113.57.63>
2017-02-07 12:40:39 -0500

    design

---
+++ HomeworkOrganizer/src/main/AssignmentPanel.java
@@ -0,0 +1,8 @@
+package main;
+
+import javax.swing.JPanel;
+
+public class AssignmentPanel extends JPanel {
+	
+	
+}
--- HomeworkOrganizer/src/main/SchedulePanel.java
+++ HomeworkOrganizer/src/main/SchedulePanel.java
@@ -2,12 +2,16 @@ package main;
 
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
+import javax.swing.JLabel;
 
 public class SchedulePanel extends JPanel {
 	
 	
 	public SchedulePanel() {
 		setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblPeriod = new JLabel("Period");
+		add(lblPeriod, BorderLayout.WEST);
 	}
 	
 	
