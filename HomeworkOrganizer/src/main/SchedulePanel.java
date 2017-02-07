04f399d7a097b58771f7dbe1a4baa2a077fc14a8
100735 <100735@10.113.57.63>
2017-02-07 13:50:20 -0500

    added the components to the schedule panel

--- HomeworkOrganizer/src/main/SchedulePanel.java
+++ HomeworkOrganizer/src/main/SchedulePanel.java
@@ -3,18 +3,75 @@ package main;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
 
 public class SchedulePanel extends JPanel {
+	private JTextField periodTextField;
+	private JTextField courseNameTextField;
+	private JTextField percentageTextField;
 	
 	
 	public SchedulePanel() {
-		setLayout(new BorderLayout(0, 0));
 		
 		JLabel lblPeriod = new JLabel("Period");
-		add(lblPeriod, BorderLayout.WEST);
+		
+		JLabel lblCourseName = new JLabel("Course Name");
+		
+		JLabel lblGradePercentage = new JLabel("Grade Percentage");
+		
+		periodTextField = new JTextField();
+		periodTextField.setToolTipText("(1, 2,3, ...)");
+		periodTextField.setColumns(20);
+		
+		courseNameTextField = new JTextField();
+		courseNameTextField.setToolTipText("(Math, English, Economics...)");
+		courseNameTextField.setColumns(20);
+		
+		percentageTextField = new JTextField();
+		percentageTextField.setToolTipText("(65.48, 84.93, 99.99, ...)");
+		percentageTextField.setColumns(20);
+		
+		GroupLayout groupLayout = new GroupLayout(this);
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblGradePercentage)
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(percentageTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblPeriod, GroupLayout.PREFERRED_SIZE, 67, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblCourseName))
+							.addGap(45)
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(courseNameTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+								.addComponent(periodTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
+					.addContainerGap(185, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblPeriod, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+						.addComponent(periodTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblCourseName)
+						.addComponent(courseNameTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(19)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblGradePercentage)
+						.addComponent(percentageTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(196, Short.MAX_VALUE))
+		);
+		setLayout(groupLayout);
 	}
-	
-	
-	
-
 }
