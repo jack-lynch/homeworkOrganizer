5d5ac43e2066a1be17ed328695488a3ee064548f
100735 <100735@10.113.57.63>
2017-02-08 12:11:38 -0500

    added calendar back in

---
+++ HomeworkOrganizer/src/Tester/assignmentChecker.java
@@ -0,0 +1,22 @@
+package Tester;
+
+import main.Classwork;
+import main.Test;
+
+public class assignmentChecker {
+	
+	public static void main(String[] args) {
+<<<<<<< HEAD
+//		Test t = new Test("History","Homework", 10 , 4, 1999);
+//		Classwork c = new Classwork("History","Homework", 10 , 4, 1999);
+//		System.out.println(t.toString());
+//		System.out.println(c.toString());
+=======
+		//Test t = new Test("History","Homework", 10 , 4, 1999);
+		//Classwork c = new Classwork("History","Homework", 10 , 4, 1999);
+		//System.out.println(t.toString());
+		//System.out.println(c.toString());
+>>>>>>> branch 'master' of https://github.com/jack-lynch/homeworkOrganizer.git
+		
+	}
+}
